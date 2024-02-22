a8b4b082240aaaceac345e37f9f22e23d56705e5
guvenlikoruma <64745731+guvenlikoruma@users.noreply.github.com>
2024-02-22 13:34:03 +0300

    Update _autoReply.js

--- plugins/_autoReply.js
+++ plugins/_autoReply.js
@@ -5,8 +5,8 @@ export async function all(m) {
    this.sendMessage(m.chat,{text:`Hello @${m.sender.split('@')[0]}\nyou can rent the bot to join a group\n\n_For more info you can DM the owner_\n*Type* \`\`\`.owner\`\`\` *to contact the owner*`.trim()}, {quoted:m});
    /*this.sendButton(m.chat, `*Invite bot to a group*      
     Hallo @${m.sender.split('@')[0]} 
-    you can rent the bot to join a group or contact owner 
-    more info click on the button
+    Bir gruba katılmak veya sahibiyle iletişime geçmek için botu kiralayabilirsiniz. 
+    daha fazla bilgi için butona tıklayın
   `.trim(), igfg, null, [['Rent', '/buyprem']] , m, { mentions: [m.sender] })*/
    m.react('💎')
 } 
